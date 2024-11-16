33cda3ec54505d39f9e6a9ca8e368e0f3f53b0ae
HajarElMannani <laso125@hotmail.com>
2024-11-16 00:59:38 -0800

    First commit

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel():
     '''class that defines all common attributes/methods for other classes'''
     def __init__(self, *args, **kwargs):
@@ -24,22 +25,22 @@ class BaseModel():
                         value = datetime.strptime(value, date_format)
                     setattr(self, key, value)
         else:
-            models.storage.new()
+            models.storage.new(self)
 
     def __str__(self):
         '''return string representation
         Return: string representation'''
-        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
         '''updates the public instance attribute updated_at with the current datetime
         Return: Current datetime'''
-        models.storage.save()
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         '''returns a dictionary containing all keys/values of __dict__ of the instance
         Return: dictionary'''
-        inst_dict = {**self.__dict__, '__class__': __class__.__name__, 'created_at': self.updated_at.isoformat(), 'updated_at': self.updated_at.isoformat()}
+        inst_dict = {**self.__dict__, '__class__': self.__class__.__name__, 'created_at': self.updated_at.isoformat(), 'updated_at': self.updated_at.isoformat()}
         return inst_dict
 
     
