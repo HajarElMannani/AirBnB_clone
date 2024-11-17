5c46f4ecc65ef20be629ae0bdde655e9b302cd4c
HajarElMannani <laso125@hotmail.com>
2024-11-17 00:02:26 -0800

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ import models
 
 class BaseModel():
     '''class that defines all common attributes/methods for other classes'''
+
     def __init__(self, *args, **kwargs):
         '''instantiation of class BaseModel
         args:
